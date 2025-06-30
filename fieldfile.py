4bd16d583c300a3e19ab9deb5dc6bd1049cb7c06
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-06-30 15:30:12 +0200

    Updated Geometry.__eq__

--- fieldfile.py
+++ fieldfile.py
@@ -21,23 +21,27 @@ class Geometry:
     def __str__(self) -> str:
         return f"{self.nr} {self.ns} {self.nz} {self.nel}"
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> tuple[bool, str]:
+        if not isinstance(other, Geometry):
+            return False, "Not a Geometry instance"
+
         is_equal = True
-        msg = ""
+        msg = []
+
         if self.nr != other.nr:
-            msg += " nr differs"
+            msg.append("nr differs")
             is_equal = False
         if self.ns != other.ns:
-            msg += " ns differs"
+            msg.append("ns differs")
             is_equal = False
         if self.nz != other.nz:
-            msg += " nz differs"
+            msg.append("nz differs")
             is_equal = False
         if self.nel != other.nel:
-            msg += " nel differs"
+            msg.append("nel differs")
             is_equal = False
 
-        return is_equal, msg
+        return is_equal, ", ".join(msg)
 
 
 class Header:
