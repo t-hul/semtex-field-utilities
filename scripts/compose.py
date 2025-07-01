14d369610a5ec0b22f5b86f47798a0c3796b037d
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-07-01 19:03:11 +0200

    compose_single updated and tested
    compose_dual still WIP

--- scripts/compose.py
+++ scripts/compose.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin python
 #
 # Interactive field file composition utility to combine two field
 # files, with field selection.  Alternatively, given a single field
@@ -18,78 +18,64 @@
 # ----------------------------------------------------------------------------
 
 import argparse
-import re
-import sys
+import logging
 
-import fieldfile
 import numpy as np
 
-if len(sys.argv) == 2:
-    mode = 1
-    file1 = fieldfile.Fieldfile(sys.argv[1], "r")
-elif len(sys.argv) == 3:
-    mode = 2
-    file1 = fieldfile.Fieldfile(sys.argv[1], "r")
-    file2 = fieldfile.Fieldfile(sys.argv[2], "r")
-    if (
-        (file1.hdr.geometry.nr != file2.hdr.geometry.nr)
-        or (file1.hdr.geometry.ns != file2.hdr.geometry.ns)
-        or (file1.hdr.geometry.nz != file2.hdr.geometry.nz)
-        or (file1.hdr.geometry.nel != file2.hdr.geometry.nel)
-    ):
-        print("The two input files do not conform")
-        sys.exit(1)
-else:
-    print("Usage: compose.py file.fld [anotherfile.fld]")
-    sys.exit(1)
+from semtex_fieldio.fieldfile import Fieldfile
 
-if mode == 1:
-    print("Input has these fields, supply a string below to")
-    print("indicate/rename ones you want:")
-    print(file1.hdr.fields)
+logger = logging.getLogger(__name__)
+
+
+def compose_single(file1: Fieldfile):
+    print(
+        "Input has these fields. Supply a string below to indicate/rename ones you want. Separate fields by comma ','. Indicate unwanted fields by whitespace.:"
+    )
+    print(",".join(file1.fields))
     required_fields = input()
-    if len(required_fields) < len(file1.hdr.fields):
-        print(required_fields, ": cannot shorter than :", file1.hdr.fields)
-        sys.exit(1)
-    wanted = "".join(re.findall(r"\S+", required_fields))
+    wanted = required_fields.split(",")  # should result in list of field names
+    if len(wanted) < len(file1.fields):
+        print(
+            f"{required_fields}: must have same or higher number of fields :{file1.fields}"
+        )
+        exit(1)
 
-    outfile = input("type in an output file name: ")
+    outfile_name = input("type in an output file name: ")
     newhdr = file1.hdr
-    newhdr.fields = wanted
-    ofile = fieldfile.Fieldfile(outfile, "w", newhdr)
+    newhdr.fields = [f.strip() for f in wanted if not f.isspace()]
+    ofile = Fieldfile(outfile_name, "w", newhdr)
 
-    ofile.data = np.zeros((len(ofile.hdr.fields), ofile.ntot))
+    print(newhdr.fields)
+    ofile.data = np.zeros((len(ofile.fields), ofile.npoints))
 
     j = 0
-    diff = 0
+    # copy wanted fields to new file
+    logger.debug(f"shape of file1.data: {file1.data.shape}")
+    logger.debug(f"shape of ofile.data: {ofile.data.shape}")
     for i, field in enumerate(file1.fields):
-        buf = file1.f.read(file1.ntot * 8)
-        #        j = required_fields.find(field)
-        print("field: " + field + ", j: " + str(j))
-        if (required_fields[i].isspace()) == False:
-            print("size: " + str(ofile.data.shape))
-            ofile.data[j] = np.fromstring(buf, np.float64, -1)
-            if not (i + diff == j):
-                for d in range(i, j):
-                    value = input(
-                        "type const value for field " + required_fields[d] + ": "
-                    )
-                    ofile.data[d, :] = value
-                    diff += 1
-                j += 1
+        logger.debug(f"field[{i}]: {field}")
+        if not wanted[i].isspace():
+            logger.debug(f"copy to ofile.field[{j}]: {ofile.fields[j]}")
+            ofile.data[j] = file1.data[i]  # copy data based on index of field, not name
+            j += 1  # index of ofile.fields
+    # fill additional fields with constant values
+    if j < len(ofile.fields):
+        for idx in range(j, len(ofile.fields)):
+            value = input(f"type const value for field {ofile.fields[idx]}: ")
+            ofile.data[idx, :] = value
 
     ofile.write(ofile.data)
-    file1.close()
-    ofile.close()
 
-else:
-    print("Input1 has these fields, supply a string below to")
-    print("indicate/rename ones you want:")
+
+def compose_dual(file1: Fieldfile, file2: Fieldfile):
+    print(
+        "Input1 has these fields, supply a string below to indicate/rename ones you want:"
+    )
     print(file1.hdr.fields)
     required_fields1 = input()
     if len(required_fields1) < len(file1.hdr.fields):
         print(required_fields1, ": cannot be shorter than :", file1.hdr.fields)
-        sys.exit(1)
+        exit(1)
     wanted1 = "".join(re.findall(r"\S+", required_fields1))
 
     print("Input2 has these fields, supply a string below to")
@@ -98,14 +84,14 @@ else:
     required_fields2 = input()
     if len(required_fields2) < len(file2.hdr.fields):
         print(required_fields2, ": cannot be shorter than :", file2.hdr.fields)
-        sys.exit(1)
+        exit(1)
     wanted2 = "".join(re.findall(r"\S+", required_fields2))
 
     outfile = input("type in an output file name: ")
     newhdr = file1.hdr
     newhdr.fields = wanted1 + wanted2
     print("new file will have fields: ", newhdr.fields)
-    ofile = fieldfile.Fieldfile(outfile, "w", newhdr)
+    ofile = Fieldfile(outfile, "w", newhdr)
 
     ofile.data = np.zeros((len(ofile.hdr.fields), ofile.ntot))
 
@@ -125,3 +111,31 @@ else:
     file1.close()
     file2.close()
     ofile.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Compose Semtex field files")
+    parser.add_argument("file1_name", help="First input field file")
+    parser.add_argument(
+        "file2_name", nargs="?", help="Second input field file (optional)"
+    )
+    args = parser.parse_args()
+
+    mode = 1 if args.file2_name is None else 2
+
+    file1 = Fieldfile(args.file1_name, "r")
+
+    if mode == 2:
+        file2 = Fieldfile(args.file2_name, "r")
+        if not file1.has_equal_geometry(file2.geometry):
+            print("The two input files do not conform")
+            exit(1)
+
+    if mode == 1:
+        compose_single(file1)
+    else:
+        compose_dual(file1, file2)
+
+
+if __name__ == main():
+    main()
