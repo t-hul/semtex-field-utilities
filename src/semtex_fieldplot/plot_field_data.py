969f773f1d285f5af339d29e1166bfc0afdd6de8
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-07-07 18:38:50 +0200

    Do normalization and labelling using utilities from plot_utils

--- src/semtex_fieldplot/plot_field_data.py
+++ src/semtex_fieldplot/plot_field_data.py
@@ -3,6 +3,7 @@ import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import plot_utils.config as conf
+import plot_utils.processor as proc
 import plot_utils.utils as utils
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
@@ -55,13 +56,12 @@ def plot_from_config(file_names, config, save_path):
 
     mesh = Mesh.from_file(mesh_name)
 
-    config.copy
-
     for file_idx, fname in enumerate(file_names):
         ff = Fieldfile(fname, "r")
         field_list = config.get("fields")
         if field_list:
             field_list = [f for f in field_list if f in ff.fields]
+        scaling_params = utils.get_scaling_parameters(config, None, 1)
 
         fig, axs = plt.subplots(
             len(field_list),
@@ -78,6 +78,11 @@ def plot_from_config(file_names, config, save_path):
             raise RuntimeError("Geometry mismatch")
 
         data1 = ff.read_zplane(z_idx, field_names=field_list)
+        data_dict = ff.get_data_dict(data1, field_names=field_list)
+        data_dict = proc.normalize_data(
+            data_dict, config.get("normalize"), **scaling_params
+        )[0]
+        data1 = np.vstack([data_dict[name] for name in field_list])
         cmap = config.get("color_map", "viridis")
 
         if config.get("dual_plane", False):
@@ -86,6 +91,11 @@ def plot_from_config(file_names, config, save_path):
             else:
                 z_idx_2 = (z_idx + ff.geometry.nz // 2) % ff.geometry.nz
                 data2 = ff.read_zplane(z_idx_2, field_names=field_list)
+                data_dict2 = ff.get_data_dict(data2, field_names=field_list)
+                data_dict2 = proc.normalize_data(
+                    data_dict2, config.get("normalize"), **scaling_params
+                )[0]
+                data2 = np.vstack([data_dict2[name] for name in field_list])
         else:
             data2 = None
 
@@ -94,18 +104,31 @@ def plot_from_config(file_names, config, save_path):
                 d2 = data2[i]
             else:
                 d2 = None
+
+            if config.get("normalize"):
+                label = proc.get_axis_label(
+                    field, config.get("normalize"), config.get("use_plus")
+                )
+            else:
+                label = field
             plot_field_contour(
                 axs[i],
                 mesh,
                 data1[i],
                 field_zplane_2=d2,
-                label=field,
+                label=label,
                 cmap=cmap,
             )
+
+            conf.set_axis_limits(axs[i], config)
+
             if config.get("plot_mesh"):
                 plot_mesh_xy_symm(axs[i], mesh, only_elements=True)
             if i < len(field_list) - 1:
                 axs[i].set_xlabel("")
+
+        if isinstance(config.get("title"), list):
+            plt.title(rf"{config.get('title')[file_idx]}")
 
         plt.savefig(f"{save_path}/contour_{figure_name}_{file_idx}.pdf", format="pdf")
         plt.savefig(f"{save_path}/contour_{figure_name}_{file_idx}")
