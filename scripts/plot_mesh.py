85597590d1cf53e3eabcd48b50035b8019394f24
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2025-07-07 11:09:50 +0200

    labelling moved to plot_mesh script

--- scripts/plot_mesh.py
+++ scripts/plot_mesh.py
@@ -1,6 +1,15 @@
+import matplotlib.pyplot as plt
+
 from semtex_fieldio import Mesh
 from semtex_fieldplot.plot_mesh import plot_mesh_xy, plot_mesh_xy_symm
+
+ax, fig = plt.subfigures()
 
 mesh = Mesh.from_file("tests/data/koal1.msh")
 plot_mesh_xy(mesh)
 plot_mesh_xy_symm(mesh)
+
+ax.set_aspect("equal")
+ax.set_title("Mesh in xy-plane")
+plt.xlabel("x")
+plt.ylabel("r")
