20d001fc41be649da7d7e58511bbcdec780160d6
Thomas Hultsch <thomas.hultsch@tu-dresden.de>
2026-06-20 17:37:26 +0200

    Simple implementation of semtex_to_untructured_grid()

--- src/semtex_fieldio/vtk_export.py
+++ src/semtex_fieldio/vtk_export.py
@@ -1,11 +1,28 @@
-def mesh_to_vtk_points(mesh):
+import numpy as np
+import pyvista as pv
+from .mesh import Mesh
+from .fieldfile import Fieldfile
+
+
+def mesh_to_vtk_points(mesh: Mesh) -> np.ndarray:
     ...
 
-def mesh_to_vtk_cells(mesh, order="linearized"):
+
+def mesh_to_vtk_cells(mesh: Mesh) -> tuple[np.ndarray, np.ndarray]:
     ...
 
-def field_to_point_data(fieldfile):
+
+def field_to_point_data(fieldfile: Fieldfile) -> dict[str, np.ndarray]:
     ...
 
-def semtex_to_unstructured_grid(mesh, fieldfile):
-    ...
+
+def semtex_to_unstructured_grid(mesh: Mesh, fieldfile: Fieldfile) -> pv.UnstructuredGrid:
+    points = mesh_to_vtk_points(mesh)
+    cells, celltypes = mesh_to_vtk_cells(mesh)
+
+    grid = pv.UnstructuredGrid(cells, celltypes, points)
+
+    for name, values in field_to_point_data(fieldfile).items():
+        grid.point_data[name] = values
+
+    return grid
